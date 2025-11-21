500fb5e57a6119b104c7d3d060065979750fe938
tanapat_ONg <150206065+tanapat23@users.noreply.github.com>
2025-11-21 23:05:49 +0700

    Add files via upload

---
+++ lab/lab01/lab1_7/src/lab1_7/lab1_7.java
@@ -0,0 +1,42 @@
+package lab1_7;
+import java.util.Scanner;
+public class lab1_7 {
+
+    public static void main(String[] args) {
+        int countN = 0;
+        int sum = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            while (countN <= 0) {
+                System.out.print("Please enter the count: ");
+                if (sc.hasNextInt()) {
+                    countN = sc.nextInt();
+                    if (countN <= 0) {
+                        System.out.println("N must 0");
+                    }
+                } else {
+                    System.out.println("Please enter an integer number.");
+                    sc.next();
+                }
+            }
+            for (int i = 1; i <= countN; i++) {
+                int currentNumber = 0;
+                boolean validNumber = false;
+                while (!validNumber) {
+                    System.out.print("Enter number " + i + " of " + countN + ": ");
+                    
+                    if (sc.hasNextInt()) {
+                        currentNumber = sc.nextInt();
+                        validNumber = true;
+                    } else {
+                        System.out.println("Error: Invalid input. Please enter an integer number.");
+                        sc.next();
+                    }
+                }
+                
+                sum += currentNumber;
+            }
+            System.out.println(countN + " numbers is: " + sum);
+            
+        }
+    }
+}
