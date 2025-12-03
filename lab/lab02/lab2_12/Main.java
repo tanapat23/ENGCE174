c678fbb0c46caaf22cc5b86889028db8044ef994
tanapat_ONg <150206065+tanapat23@users.noreply.github.com>
2025-12-03 22:28:22 +0700

    Add files via upload

--- lab/lab02/lab2_12/Main.java
+++ lab/lab02/lab2_12/Main.java
@@ -1,5 +1,34 @@
 package lab2_12;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        
+        String courseId;
+        String courseName;
+        String studentName;
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            
+            courseId = getValidString(sc, "Enter Course ID: ");
+            
+            courseName = getValidString(sc, "Enter Course Name: ");
+            
+            studentName = getValidString(sc, "Enter Student Name: ");
+            
+            Course oopCourse = new Course(courseId, courseName);
+            
+            Student student1 = new Student(studentName, oopCourse);
+            
+            student1.displayEnrollment();
+            
+        } 
+    }
+    
+    public static String getValidString(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
 }
